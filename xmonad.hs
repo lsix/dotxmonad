9b2674020c9ca16825d0c9e502d08341743532d5
Lancelot SIX <lancelot.six@quiet-oceans.com>
2015-05-06 10:58:35 +0200

    Add grid layout

--- xmonad.hs
+++ xmonad.hs
@@ -1,18 +1,26 @@
 import XMonad
 import XMonad.Config.Azerty
 import XMonad.Hooks.DynamicLog
+import XMonad.Layout.Grid
 
 import Data.Monoid
 import qualified Data.Map as M
+
+main :: IO ()
+main = statusBar myBar myPP toggleStrutsKey myConfig  >>=
+       xmonad
+
+myConfig = defaultConfig { keys = \c -> azertyKeys c `M.union` keys defaultConfig c
+			 , terminal = "konsole"
+			 , workspaces = ["1","2","3","4","5","6","chat","mail","web"]
+			 , manageHook = manageHook defaultConfig <+> myManageHook
+			 , layoutHook = myLayoutHook
+			 }
 
 myManageHook :: Query (Data.Monoid.Endo WindowSet)
 myManageHook = composeAll
 	[ className =? "qemu-system-x86_64" --> doFloat
  	]
-
-main :: IO ()
-main = statusBar myBar myPP toggleStrutsKey myConfig  >>=
-       xmonad
 
 myPP = xmobarPP { ppOutput = putStrLn
 		, ppCurrent = xmobarColor "orange" "" . wrap "[" "]"
@@ -25,8 +33,4 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myBar = "/home/lsix/.nix-profile/bin/xmobar"
 
-myConfig = defaultConfig { keys = \c -> azertyKeys c `M.union` keys defaultConfig c
-			 , terminal = "konsole"
-			 , workspaces = ["1","2","3","4","5","6","chat","mail","web"]
-			 , manageHook = manageHook defaultConfig <+> myManageHook
-			 }
+myLayoutHook = layoutHook defaultConfig ||| Grid
